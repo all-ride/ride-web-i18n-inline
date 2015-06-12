f3fde5be58a6737f659700cf75f3cb3f6f759f07
Brent Roose <brent.roose@gmail.com>
2015-06-12 09:14:55 +0200

    Code refactoring

--- src/ride/library/i18nquickadmin/listener/ApplicationListener.php
+++ src/ride/library/i18nquickadmin/listener/ApplicationListener.php
@@ -46,13 +46,13 @@ class ApplicationListener {
             return;
         }
 
-        $taskbar = $event->getArgument('taskbar');
-        $applicationMenu = $taskbar->getApplicationsMenu();
-        $referer = '?referer=' . urlencode($request->getUrl());
         $user = $securityManager->getUser();
         $toggle = $user->getPreference('translator') ? "disable" : "enable";
+        $referer = '?referer=' . urlencode($request->getUrl());
         $url = $web->getUrl('l10n.api.translator.toggle', array()) . $referer;
 
+        $taskbar = $event->getArgument('taskbar');
+        $applicationMenu = $taskbar->getApplicationsMenu();
         $menuItem = new MenuItem();
         $menuItem->setTranslation('translator.toggle.' . $toggle);
         $menuItem->setUrl($url);
