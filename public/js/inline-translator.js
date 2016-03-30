db2c2bcb42033d228805dcc3baa9c987903c5354
Brent Roose <brent.roose@gmail.com>
2016-03-30 15:52:58 +0200

    Add eventlisteners and keybindings for convenience, and change some theming

--- public/js/inline-translator.js
+++ public/js/inline-translator.js
@@ -1,6 +1,5 @@
 
-$(document).ready(function() {
-    // Initialize the translator
+document.addEventListener('DOMContentLoaded', function() {
     InlineTranslatorAPI.init('/api/v1/i18n');
     TranslationCollection.init();
 });
@@ -49,11 +48,31 @@ var InlineTranslatorAPI = {
     },
 };
 
+/**
+ * A translation
+ * @type {Object}
+ */
 var Translation = {
+    /**
+     * @type {String}
+     */
     'key': null,
+
+    /**
+     * @type {String}
+     */
     'currentLocale': null,
+
+    /**
+     * @type {String}
+     */
     'text': null,
 
+    /**
+     * Create and return a new Translation from a DOM element
+     * @param  {DOMElement} el
+     * @return {Translation}
+     */
     'create': function(el) {
         var translation = Object.create(Translation);
 
@@ -62,107 +81,67 @@ var Translation = {
         return translation;
     },
 
+    /**
+     * Initialize a new Translation
+     * @param  {DOMElement} el
+     */
     'init': function(el) {
         this.key = el.getAttribute('data-translation-key');
         this.currentLocale = el.getAttribute('data-locale');
         this.text = el.innerHTML;
     },
 
+    /**
+     * Save this translation
+     * @param  {object} values
+     * @return {Promise}
+     */
     'save': function(values) {
-        var self = this;
-
-        InlineTranslatorAPI.post(this.currentLocale, this.key, values).then(function() {
-            var text = values[self.currentLocale];
-
-            $('mark.inline_translation[data-translation-key="' + self.key + '"]').text(text);
-            $('.translation_list li[data-translation-key="' + self.key + '"] span').text(text);
-        });
+        return InlineTranslatorAPI.post(this.currentLocale, this.key, values);
     },
 
-    'edit': function() {
-        InlineTranslatorAPI.get(this.key).then(function(json) {
-            var $form = $('.translation_form'),
-                $rows = $('.translation_form--rows'),
-                $row;
-
-            $.each(json, function(locale, data) {
-                $row = $('<div><label for="'+locale+'-'+data.key+'">'+locale+'</label><input type="text" id="'+locale+'-'+data.key+'" data-locale="'+locale+'"/></div>');
-                if (data.translation) {
-                    $row.find('input').val(data.translation);
-                }
-
-                $rows.append($row);
-            });
-
-            $form.addClass('active');
-        });
-    },
+    'highlight': function(enable) {
+        if (enable === undefined || enable) {
+            $('mark.inline_translation[data-translation-key="' + this.key + '"]').addClass('inline_translation--active');
+        } else {
+            $('mark.inline_translation[data-translation-key="' + this.key + '"]').removeClass('inline_translation--active');
+        }
+    }
 };
 
 var TranslationCollection = {
     'translations': {},
     'el': null,
+    'form': null,
+    'rows': null,
+    'translationEdit': null,
+    'promise': null,
 
     'init': function() {
         var translation = null,
+            $labels = $('mark.inline_translation'),
             self = this;
 
-        $('mark.inline_translation').each(function(k, el) {
+        if (!$labels.length) {
+            return;
+        }
+
+        $labels.each(function(k, el) {
             translation = Translation.create(el);
 
             self.translations[translation.key] = translation;
         });
 
-        this.render();
-    },
+        document.addEventListener('keydown', function(e) {
+            if (!e.altKey) {
+                return;
+            }
 
-    'render': function() {
-        this.el = $('<div class="translation_list"><ul></ul></div>');
-        var self = this,
-            $translationList = this.el.find('ul'),
-            $form = $('<div class="translation_form"><div class="translation_form--rows"></div>'),
-            $rows = $form.find('.translation_form--rows'),
-            $save = $('<a href="#" class="translation_form--save btn">Save</a>');
-
-        $.each(this.translations, function(k, translation) {
-            var $translationListItem = $('<li></li>');
-
-            $translationListItem.html('<span>' + translation.text + '</span>' + '<small>' + translation.key + '</small>');
-            $translationListItem.attr('data-translation-key', translation.key);
-            $translationList.append($translationListItem);
-
-            $translationListItem.on('mouseenter', function() {
-                $('mark.inline_translation[data-translation-key="' + translation.key + '"]').addClass('inline_translation--active');
-            });
-
-            $translationListItem.on('mouseleave', function() {
-                $('mark.inline_translation[data-translation-key="' + translation.key + '"]').removeClass('inline_translation--active');
-            });
-
-            $translationListItem.on('click', function() {
-                $form.data('translation-key', translation.key);
-                $rows.empty();
-
-                translation.edit();
-            });
+            $labels.addClass('pointer');
         });
 
-        $form.append($save);
-        $translationList.append($form);
-
-        $save.on('click', function(e) {
-            e.preventDefault();
-
-            var translation = self.translations[$form.data('translation-key')],
-                values = {};
-
-            $.each($rows.find('input'), function(k, row) {
-                values[row.getAttribute('data-locale')] = row.value;
-            });
-
-            translation.save(values);
-
-            $form.removeClass('active');
+        document.addEventListener('keyup', function(e) {
+            $labels.removeClass('pointer');
         });
 
         $('mark.inline_translation').on('click', function(e) {
@@ -173,26 +152,150 @@ var TranslationCollection = {
             e.preventDefault();
             e.stopPropagation();
 
-            var translation = self.translations[this.getAttribute('data-translation-key')];
-            $form.data('translation-key', translation.key);
-            $rows.empty();
-
-            translation.edit();
+            self.openForm(self.translations[this.getAttribute('data-translation-key')]);
         });
 
-        document.addEventListener('keydown', function(e) {
-            if (!e.altKey) {
-                return;
-            }
-
-            $('mark.inline_translation').addClass('pointer');
-        });
-
-        document.addEventListener('keyup', function(e) {
-            $('mark.inline_translation.pointer').removeClass('pointer');
-        });
-
-        $('body').append(this.el);
+        $('body').append(this.renderList());
     },
 
+    'renderList': function() {
+        this.el = $('<div class="translation_list"><ul></ul></div>');
+
+        var self = this,
+            $translationList = this.el.find('ul');
+
+        $.each(this.translations, function(k, translation) {
+            var $translationListItem = $('<li></li>');
+
+            $translationListItem.html('<span>' + translation.text + '</span>' + '<small>' + translation.key + '</small>');
+            $translationListItem.attr('data-translation-key', translation.key);
+            $translationList.append($translationListItem);
+
+            $translationListItem.on('mouseenter', function() {
+                translation.highlight();
+            });
+
+            $translationListItem.on('mouseleave', function() {
+                translation.highlight(false);
+            });
+
+            $translationListItem.on('click', function() {
+                $translationListItem.off('mouseleave');
+                self.openForm(translation);
+            });
+        });
+
+        this.el.append(this.renderForm());
+
+        return this.el;
+    },
+
+    'renderForm': function() {
+        var self = this,
+            $save = $('<a href="#" class="translation_form--save btn">Save</a>'),
+            $cancel = $('<a href="#" class="translation_form--cancel">Cancel</a>');
+
+        this.form = $('<div class="translation_form"><h3></h3><div class="translation_form--rows"></div><div class="translation_form--actions"></div></div>');
+        this.rows = this.form.find('.translation_form--rows');
+        this.formActions = this.form.find('.translation_form--actions');
+
+        this.formActions.append($save);
+        this.formActions.append($cancel);
+
+        $save.on('click', function(e) {
+            e.preventDefault();
+            self.saveForm(self.translations[self.form.data('translation-key')])
+        });
+
+        $cancel.on('click', function(e){
+            e.preventDefault();
+            self.closeForm();
+        });
+
+        return this.form;
+    },
+
+    'openForm': function(translation) {
+        if (!translation) {
+            return;
+        }
+
+        this.promise = $.Deferred();
+
+        var self = this,
+            key = translation.key;
+
+        this.form.data('translation-key', key);
+        this.form.find('h3').text(key);
+        this.rows.empty();
+
+        InlineTranslatorAPI.get(key).then(function(json) {
+            self.translationEdit = translation;
+            self.promise.resolve();
+            var $row;
+
+            $.each(json, function(locale, data) {
+                $row = $('<div><label for="'+locale+'-'+data.key+'">'+locale+'</label><input type="text" id="'+locale+'-'+data.key+'" data-locale="'+locale+'"/></div>');
+                if (data.translation) {
+                    $row.find('input').val(data.translation);
+                }
+
+                $row.on('keydown', function(e) {
+                    if (e.keyCode === 13) {
+                        self.saveForm(translation);
+                    } else if (e.keyCode === 27) {
+                        self.closeForm();
+                    }
+                });
+
+                self.rows.append($row);
+            });
+
+            self.el.addClass('edit');
+            self.rows.find('input').first().focus().select();
+            translation.highlight();
+        });
+    },
+
+    'saveForm': function() {
+        var self = this,
+            values = {};
+
+        $.when(this.promise).then(function() {
+            $.each(self.rows.find('input'), function(k, row) {
+                values[row.getAttribute('data-locale')] = row.value;
+            });
+
+            self.translationEdit.save(values).then(function() {
+                var text = values[self.translationEdit.currentLocale],
+                    $translationListItem = $('.translation_list li[data-translation-key="' + self.translationEdit.key + '"]');
+
+                $('mark.inline_translation[data-translation-key="' + self.translationEdit.key + '"]').text(text);
+                $translationListItem.find('span').text(text);
+
+                self.closeForm();
+            });
+        });
+    },
+
+    'closeForm': function() {
+        var self = this;
+
+        $.when(this.promise).then(function() {
+            $translationListItem = $('.translation_list li[data-translation-key="' + self.translationEdit.key + '"]');
+
+            self.translationEdit.highlight(false);
+            self.el.removeClass('edit');
+
+            self.translationEdit = null;
+
+            $translationListItem.on('mouseleave', function() {
+                var translation = self.translations[$translationListItem.data('translation-key')];
+
+                if (translation) {
+                    translation.highlight(false);
+                }
+            });
+        });
+    },
 };
